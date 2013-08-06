08c0a97c89d58ae1da2a1d204affb62fa71b39f1
David Wolever <david@wolever.net>
2013-08-05 20:41:49 -0400

    Updating setup.py with description and author info and stuff

--- setup.py
+++ setup.py
@@ -7,13 +7,26 @@ from setuptools import setup, find_packages
 
 import browsercast
 
+try:
+    long_description = open("README.rst", "U").read()
+except IOError:
+    long_description = "See https://github.com/wolever/browsercast"
+
 version = "%s.%s.%s" %browsercast.__version__
 setup(
     name="browsercast",
     version=version,
     url="https://github.com/wolever/browsercast",
+    author="David Wolever",
+    author_email="david@wolever.net",
     packages=find_packages(),
     include_package_data=True,
+    description="""
+        An IPython Notebook plugin which allows IPython Notebook notebooks to
+        be set to audio, creating a screencast-style presentation in the
+        browser - a browser cast.
+    """,
+    long_description=long_description,
     zip_safe=False,
     license="BSD",
     classifiers=[ x.strip() for x in """
